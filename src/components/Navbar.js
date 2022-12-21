1511cd4d8696795c33f4530ed725360faacd781e
Franks555 <63602861+Franks555@users.noreply.github.com>
2022-12-21 14:46:18 -0300

    primer commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import logo from './images/logo.png'
+
+function Navbar() {
+    return ( 
+        <header>
+            <nav className="nav">
+                <img src={logo} alt="Logo de lavado de automovil" />
+                <ul>
+                    <li><a href="/#">INICIO</a></li>
+                    <li><a href="/#">QUIENES SOMOS</a></li>
+                    <li><a href="/#">PRODUCTOS</a></li>
+                    <li><a href="/#">CONTACTO</a></li>
+                </ul>
+            </nav>
+        </header>
+
+    );
+}
+
+export default Navbar;
